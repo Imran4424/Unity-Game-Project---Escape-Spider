c5e3d98bd7edae9599ea37436b8a1492b9bd0915
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-10-29 18:41:16 +0600

    escape spider

--- Assets/Scripts/Spider Scripts/Spider Jumper/SpiderJumper.cs
+++ Assets/Scripts/Spider Scripts/Spider Jumper/SpiderJumper.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpiderJumper : MonoBehaviour {
+public class SpiderJumper : MonoBehaviour 
+{
+
+	private float forceY;
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	void Awake()
+	{
+		
+	}
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 }
