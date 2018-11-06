c43c311ca17060d7e3243c76006bbc8ee15811a3
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-07 00:01:49 +0600

    escape spider

--- Assets/Scripts/Controller Scripts/LevelMenuConroller.cs
+++ Assets/Scripts/Controller Scripts/LevelMenuConroller.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelMenuControllerScript : MonoBehaviour {
+public class LevelMenuConroller : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
