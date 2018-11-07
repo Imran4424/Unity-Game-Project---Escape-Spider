5595bac9876fbad90e4478c1d7e819c80fca00dc
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-07 15:57:51 +0600

    escape spider

--- Assets/Scripts/Camera Script/CameraFollow.cs
+++ Assets/Scripts/Camera Script/CameraFollow.cs
@@ -15,6 +15,14 @@ public class CameraFollow : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-				
+		if (player != null)
+		{
+			Vector3 temp = transform.position;
+
+			temp.x = player.position.x;
+			temp.y = player.position.y;
+
+			transform.position = temp;
+		}		
 	}
 }
