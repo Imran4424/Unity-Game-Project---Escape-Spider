6121da2162aa932b3623f2153b3e22ac615e73d6
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-07 06:09:06 +0600

    escape spider

--- Assets/Scripts/Controller Scripts/LevelMenuController.cs
+++ Assets/Scripts/Controller Scripts/LevelMenuController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelMenuController : MonoBehaviour 
 {
 
-	
+	public void Back()
+	{
+		SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+	}
 }
