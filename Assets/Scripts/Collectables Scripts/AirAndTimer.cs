7821918c7c64ac263fdc52e7cd1a6fbc830f00f9
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-14 22:43:58 +0600

    escape spider - UI HUD

--- Assets/Scripts/Collectables Scripts/AirAndTimer.cs
+++ Assets/Scripts/Collectables Scripts/AirAndTimer.cs
@@ -2,8 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AirAndTimer : MonoBehaviour 
+public class AirAndTimer : MonoBehaviour
 {
 
-	
+	void OnTriggerEnter2D (Collider2D target)
+	{
+		if (target.tag == "Player")
+		{
+			if (gameObject.name == "Air")
+			{
+				GameObject.Find ("GameplayController").GetComponent<AirTimer> ();
+			}
+			else
+			{
+				GameObject.Find ("GameplayController").GetComponent<LevelTimer> ();
+			}
+		}
+	}
 }
