8b2cf614066670336835873033fd7d0fbc70ac5d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-15 06:41:01 +0600

    gameplay controller

--- Assets/Scripts/Controller Scripts/Gameplay Controller/GameplayController.cs
+++ Assets/Scripts/Controller Scripts/Gameplay Controller/GameplayController.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameplayController : MonoBehaviour 
+public class GameplayController : MonoBehaviour
 {
 	[SerializeField]
 	private GameObject pausePanel;
 
 	[SerializeField]
-	private Button resumeButton;
+	private Button resumeGame;
 
-	
+	public void PauseGame ()
+	{
+		Time.timeScale = 0f;
+		pausePanel.SetActive (true);
+		resumeGame.onClick.RemoveAllListeners ();
+		resumeGame.onClick.AddListener (() >= );
+	}
+
+	public void ResumeGame ()
+	{
+		Time.timeScale = 0f;
+		pausePanel.SetActive (fa);
+	}
 }
