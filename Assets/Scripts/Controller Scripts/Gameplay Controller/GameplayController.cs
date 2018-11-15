ba572e934f9404fb8c484164a57860f9fcbc33c1
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-15 06:36:06 +0600

    escape spider - UI HUD

--- Assets/Scripts/Controller Scripts/Gameplay Controller/GameplayController.cs
+++ Assets/Scripts/Controller Scripts/Gameplay Controller/GameplayController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameplayController : MonoBehaviour 
 {
+	[SerializeField]
+	private GameObject pausePanel;
 
-	
+	[SerializeField]
+	private Button resumeButton;
 }
