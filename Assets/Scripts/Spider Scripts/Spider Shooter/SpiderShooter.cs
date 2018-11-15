c5ba0e85f37f1cf78e4aca99b47250a6f1058a07
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-15 07:10:48 +0600

    gameplay controller

--- Assets/Scripts/Spider Scripts/Spider Shooter/SpiderShooter.cs
+++ Assets/Scripts/Spider Scripts/Spider Shooter/SpiderShooter.cs
@@ -2,32 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpiderShooter : MonoBehaviour {
+public class SpiderShooter : MonoBehaviour
+{
 
 	[SerializeField]
 	private GameObject bullet;
-	
+
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
-		StartCoroutine(Attack());
+		StartCoroutine (Attack ());
 	}
 
-	IEnumerator Attack()
+	IEnumerator Attack ()
 	{
-		yield return new WaitForSeconds(Random.Range(2,7));
+		yield return new WaitForSeconds (Random.Range (2, 7));
 
-		Instantiate(bullet,transform.position,Quaternion.identity);
+		Instantiate (bullet, transform.position, Quaternion.identity);
 
-		StartCoroutine(Attack());
+		StartCoroutine (Attack ());
 	}
-	
-	void OnTriggerEnter2D(Collider2D target)
+
+	void OnTriggerEnter2D (Collider2D target)
 	{
 		if (target.tag == "Player")
 		{
-			Destroy(target.gameObject);
+			Destroy (target.gameObject);
+			GetComponent<GameplayController> ().PlayerDied ();
+
 		}
 	}
-	
+
 }
