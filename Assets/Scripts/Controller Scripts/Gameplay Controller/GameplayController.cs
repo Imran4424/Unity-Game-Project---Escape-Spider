aee544f58a522d035a15ddc58e8e4baadd688d16
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-15 06:34:37 +0600

    escape spider - UI HUD

--- Assets/Scripts/Controller Scripts/Gameplay Controller/GameplayController.cs
+++ Assets/Scripts/Controller Scripts/Gameplay Controller/GameplayController.cs
@@ -2,15 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameplayController : MonoBehaviour {
+public class GameplayController : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
